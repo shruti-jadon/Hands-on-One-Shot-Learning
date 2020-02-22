ec8d7f02cea5f15e169e7c829e757c1f421d0242
shruti-jadon <shrutijadon.0992@gmail.com>
2020-02-22 01:03:49 -0800

    removing data

--- Ch04-ModelsBasedMethods/utils.py
+++ Ch04-ModelsBasedMethods/utils.py
@@ -1,15 +1,18 @@
-from scipy.misc import imread,imresize
+from scipy.misc import imresize
+from matplotlib.pyplot import imread
 from os import listdir
 from os.path import splitext
 from random import seed,shuffle
 from time import time
 from numpy import zeros
+"""
 from tensorflow import Variable,truncated_normal,constant,nn
 
 weights = lambda shape: Variable(truncated_normal(shape, stddev=0.1))
 biases = lambda shape: Variable(constant(0.1, shape=shape))
 conv2d = lambda x, W: nn.conv2d(x, W, strides=[1, 2, 2, 1], padding='SAME')
 max_pool = lambda x: nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+"""
 
 def load_image(path,shape=False):
     readed_img = imread(path)
