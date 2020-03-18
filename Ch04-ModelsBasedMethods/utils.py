bedf694dc9d53543927bde03b625d0de0022b3dd
shruti-jadon <shrutijadon.0992@gmail.com>
2020-03-17 22:20:04 -0700

    Models Based Chapter

--- Ch04-ModelsBasedMethods/utils.py
+++ Ch04-ModelsBasedMethods/utils.py
@@ -5,14 +5,6 @@ from os.path import splitext
 from random import seed,shuffle
 from time import time
 from numpy import zeros
-"""
-from tensorflow import Variable,truncated_normal,constant,nn
-
-weights = lambda shape: Variable(truncated_normal(shape, stddev=0.1))
-biases = lambda shape: Variable(constant(0.1, shape=shape))
-conv2d = lambda x, W: nn.conv2d(x, W, strides=[1, 2, 2, 1], padding='SAME')
-max_pool = lambda x: nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-"""
 
 def load_image(path,shape=False):
     readed_img = imread(path)
@@ -23,8 +15,6 @@ def load_image(path,shape=False):
 def extend_children(path,ftype=False):
     allpaths = [path+'/'+child for child in listdir(path) if '.DS_Store' not in child]
     if ftype != False:
-# remember to include the period in ftype (ie .jpg)
-# pass '' to include only folders
         ret = []
         for v in allpaths:
             if splitext(v)[1] == ftype:
